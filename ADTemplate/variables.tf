d07b47a6637652806cef46f6ea51962cac08da8b
ShwetaliBerad <shwetali_berad@yahoo.com>
2018-12-10 17:50:47 +0530

    Update variables.tf

---
+++ ADTemplate/variables.tf
@@ -0,0 +1,34 @@
+variable "user" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ad_server" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "ad_server_domain" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "computer_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "computer_domain" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "computer_description" {
+  type = "string"
+  description = "Generated"
+}
